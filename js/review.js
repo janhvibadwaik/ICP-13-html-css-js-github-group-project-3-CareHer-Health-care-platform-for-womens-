5e3e723195cd85af5f2d7e2d8431776ad216b8f6
AkashLohar718 <aakashlohar718@gmail.com>
2026-01-14 09:21:16 +0530

    js functio added for form submission

---
+++ js/review.js
@@ -0,0 +1,33 @@
+function submitForm(){
+        const container = document.getElementById("container");
+        const reviews = document.getElementById("review-card-container");
+      const name=document.getElementById("name").value;
+      const date=document.getElementById("date").value;
+      const rating = document.querySelector('input[name="rating"]:checked')?.value || 0;
+      const stars = "★".repeat(rating) + "☆".repeat(5 - rating);
+      const comment=document.getElementById("comments").value;
+
+      const reviewData={
+        "name":name,"date":date,"stars":stars,"comment":comment};
+        if(!name || !date || !rating){
+        alert("Please fill the all empty fields before submitting..." );
+        return;
+       }
+
+      localStorage.setItem("reviewData", JSON.stringify(reviewData));
+      container.innerHTML="";
+      reviews.innerHTML+=`<div class="review-card">
+        <button class="btn-delete" onclick="displayFormDeleteReview()">delete</button>
+        <div class="date">Date:${reviewData.date}</div>
+        <div class="card-name">
+            Name:${reviewData.name}
+        </div>
+        Rating:
+        <div class="card-rating">
+           ${reviewData.stars} 
+        </div>
+        <div class="card-comment">
+            comment:<br>${reviewData.comment}
+        </div>
+    </div>`;       
+}
